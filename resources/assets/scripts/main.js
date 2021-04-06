f0202871993ffc6d15b426dd67efd07f85d8ab6f
Darina <dbogunenko@splitdev.pro>
2021-04-06 18:17:18 +0300

    options

---
+++ resources/assets/scripts/main.js
@@ -0,0 +1,32 @@
+// Polyfills if needed
+// require('intersection-observer');
+
+// import external dependencies
+import './util/polyfills'
+import 'jquery'
+
+
+
+// Import everything from autoload
+import './autoload/**/*'
+
+// import local dependencies
+import Router from './util/Router';
+import common from './routes/common';
+import Detection from './util/Detection';
+import index from './routes/index';
+
+window._detector = new Detection({
+ detect: ['ie11'],
+});
+window._detector.init();
+
+// /** Populate Router instance with DOM routes */
+const routes = new Router({
+  // All pages
+  common,
+  'homePage': index,
+});
+
+// // Load Events
+jQuery(document).ready(() => routes.loadEvents());
