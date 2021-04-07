7c872cf757d3a042b3b3a6898280bdf07651faa6
Darina <bogunenkodarina@gmail.com>
2021-04-08 00:23:01 +0300

    test

--- resources/assets/scripts/routes/common.js
+++ resources/assets/scripts/routes/common.js
@@ -1,6 +1,8 @@
+import Headroom from 'headroom.js';
 export default {
   init() {
-
+    var headroom  = new Headroom(document.querySelector('header'));
+    headroom.init();
   },
 
   // JavaScript to be fired on all pages, after page specific JS is fired
