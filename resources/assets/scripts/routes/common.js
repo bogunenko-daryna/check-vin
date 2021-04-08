bc233f34bb6026d914244bbdef065504e313f967
Darina <bogunenkodarina@gmail.com>
2021-04-08 23:46:24 +0300

    trains slider

--- resources/assets/scripts/routes/common.js
+++ resources/assets/scripts/routes/common.js
@@ -1,8 +1,23 @@
 import Headroom from 'headroom.js';
+import Swiper from 'swiper/swiper-bundle';
 export default {
   init() {
     var headroom  = new Headroom(document.querySelector('header'));
     headroom.init();
+
+    var swiper = new Swiper('.swiper-container.slider-quotes', {
+      slidesPerView: 2,
+      spaceBetween: 50,
+      pagination: {
+        el: '.swiper-pagination',
+       
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
+    swiper.init();
   },
 
   // JavaScript to be fired on all pages, after page specific JS is fired
