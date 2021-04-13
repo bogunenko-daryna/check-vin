fa8f8e5ce0adce397b6eed78b31c9705b2f78a42
Darina <bogunenkodarina@gmail.com>
2021-04-14 01:04:25 +0300

    fix

--- resources/assets/scripts/routes/common.js
+++ resources/assets/scripts/routes/common.js
@@ -1,5 +1,6 @@
 import Headroom from 'headroom.js';
 import Swiper from 'swiper/swiper-bundle';
+
 export default {
   init() {
     var headroom  = new Headroom(document.querySelector('header'));
@@ -19,14 +20,39 @@ export default {
         prevEl: '.swiper-button-prev',
       },
     });
-
+    mySwiper.init();
   
   
    }
 
-   mySwiper.init();
+   
    
 
+   $('.header__language-list a').on('click', function() {
+   let el = $(this).data('language');
+   $('.active-language').text(el);
+   $('.header__language-list').removeClass('show');
+   });
+
+   $('.active-language').on('click', function() {
+     $(this).next().addClass('show');
+   })
+
+
+   $(document).mouseup(function (e){
+    var div = $('.header__language');
+    if (!div.is(e.target)
+      && div.has(e.target).length === 0) {
+        $('.header__language-list').removeClass('show');
+    }
+  });
+
+  $('.modal-btn').on('click', function() {
+     $('.modal').addClass('show');
+  });
+  $('.modal-close').on('click', function() {
+    $('.modal').removeClass('show');
+ });
     
   },
 
